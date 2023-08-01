87e03d5ce0fec5b6df5cf2fd69ca206a6fa15953
Amine <toshamine@gmail.com>
2023-08-01 16:30:23 +0100

    Build and Push To Docker

--- src/main/java/dev/chiba/ValidationApp/controller/GreetingController.java
+++ src/main/java/dev/chiba/ValidationApp/controller/GreetingController.java
@@ -12,6 +12,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -35,5 +38,10 @@ public class GreetingController {
                 .ok(greetingService.greet(greeting));
 
     }
+
+
+
+
+
 
 }
